bccf5ef67c04786b13c03d17b3c41dca1bf9821c
Ankit Kumar Rai <ankitdsa2023@gmail.com>
2025-10-01 14:06:49 +0530

    feat: Setup lint-staged

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config({
+
+    ignores: ['dist/**'], // 👈 ignore build output
+
+    languageOptions: {
+        parserOptions: {
+            project: true,
+            tsconfigRootDir: import.meta.dirname,
+        },
+    },
+    files: ["**/*.ts"],
+    extends: [
+        eslint.configs.recommended,
+        ...tseslint.configs.recommendedTypeChecked,
+    ],
+    rules: {
+        "no-console": "error",
+        quotes: ["error", "single", { allowTemplateLiterals: true }],
+    }
+})
